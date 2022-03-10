f4d7bb36b4d13ac8b3bdf43716d83269234ebc5f
Ediblelnk <peters1731@gmail.com>
2022-03-10 00:52:41 -0500

    Investigating update function

--- games/open_games/update_testing.py
+++ games/open_games/update_testing.py
@@ -1,6 +1,8 @@
 #imports and starts the moduals needed to run the program
+from turtle import back
 import pygame 
 import sys
+import os.path
 
 pygame.init()
 
@@ -11,18 +13,55 @@ factor = 120
 width, height = ratio[0]*factor, ratio[1]*factor
 screen = pygame.display.set_mode((width, height))
 
+#blit format
+image = pygame.image.load(os.path.join(os.path.dirname(__file__), r"RockSml.jpg")).convert()
+rect = pygame.Rect(-100, -100, 100, 100)
+
 #backround format
-background_color = pygame.Color('lemonchiffon2')
+background_color = pygame.Color('lemonchiffon3')
 
 #clock format
 clock = pygame.time.Clock()
 
+def load_screen():
+	"""loads the screen before getting put into display loop"""
+	global background_color, screen
+	screen.fill(background_color)
+	pygame.display.update()
+
+changed_rects = []
+def register_movement(rect: pygame.Rect):
+	"""registers rectanges of the screen as changed, to later be changed via 'update_changed()'"""
+	changed_rects.append(rect.inflate(2,2))
+
+
+def update_changed():
+	"""updates the parts of the screen that have been changed"""
+	for changed in changed_rects:
+		pygame.display.update(changed)
+	screen.fill(background_color)
+	changed_rects.clear()
+
+#draw the rock
+def move_rock(pos: tuple):
+	"""Currently moves the rock to the position of the cursor"""
+	register_movement(rect)	#register position before moving
+	rect.x, rect.y = pos[0]-0.5*rect.w, pos[1]-0.5*rect.h
+	screen.blit(image, rect)
+	register_movement(rect)	#register position after moving
+
 #display loop
+load_screen()
 while True:
 	if pygame.event.get(pygame.QUIT):
 		sys.exit()
 
+	current_mouse_states = pygame.mouse.get_pressed()
+
+	if current_mouse_states[0] == True:
+		move_rock(pygame.mouse.get_pos())
+	if current_mouse_states[1] == True:
+		image = image.convert_alpha(5)
 	#handles displaying and refreshing the screen after each evauluation
 	clock.tick()
-	screen.fill(background_color)
-	pygame.display.update()
+	update_changed()
