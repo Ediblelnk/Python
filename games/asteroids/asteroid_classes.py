e2f782a45ee00115e87a0c40dd7959775e2f16c5
Ediblelnk <peters1731@gmail.com>
2022-03-14 01:45:27 -0400

    Asteroids! 2

--- games/asteroids/asteroid_classes.py
+++ games/asteroids/asteroid_classes.py
@@ -1,5 +1,6 @@
 from pygame.math import Vector2
-from pygame import Rect
+from pygame import Rect, Surface
+from pygame.transform import rotate
 import pygame.image as Image
 import os.path
 import random
@@ -7,63 +8,86 @@ import random
 class GameObject:
 	'''Most basic form of a object in the game.'''
 
-	#image is a sprite set for this game, use specific regions of IMAGE when blitting
-	IMAGE = Image.load(os.path.join(os.path.dirname(__file__), r"asteroids_sprites.png"))
-	#IMAGE.set_colorkey(IMAGE.get_at((0,0)))
-
-	def __init__(self, pos: tuple) -> None:
-		'''Defines a position for the GameObject'''
+	def __init__(self, pos: tuple, velocity: tuple, rotation: float, rot_velocity: float) -> None:
+		'''Defines a position and rotation and velocity for the GameObject'''
 		self.pos = Vector2(pos)
+		self.velocity = Vector2(velocity)
+		self.rotation = rotation
+		self.rot_velocity = rot_velocity
+
+	def update(self, time_delta_ms: int) -> None:
+		'''Updates the position and rotation of a GameObject'''
+		self.pos += self.velocity * (time_delta_ms / 1000)
+		self.rotation += self.rot_velocity * (time_delta_ms / 1000)
 
 class Player(GameObject):
 
-	IMAGE_LOCATION = Rect(503, 242, 75, 104)
+	_IMAGE = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"player2.png"))#.convert()
+	_IMAGE.set_colorkey(_IMAGE.get_at((0, 0)))
 	
-	def __init__(self, pos: tuple) -> None:
-		'''Creates a new Player with a defined position and velocity'''
-		super().__init__(pos)
+	def __init__(self, pos: tuple, velocity: tuple, rotation: float, rot_velocity: float) -> None:
+		'''Creates a new Player with position, velocity, rotation, and rotational velocity'''
+		super().__init__(pos, velocity, rotation, rot_velocity)
+
+	def get_image(self) -> Surface:
+		'''Gets the image to use for the player, (use because of rotation)'''
+		image = rotate(self._IMAGE, self.rotation)
+		image.set_colorkey(self._IMAGE.get_at((0,0)))
+		return image
+
+	def get_pos(self) -> Vector2:
+		'''Gets the position of the player, centered, (use because of rotation)'''
+		image = self.get_image()
+		print(image)
+		return self.pos - Vector2(image.get_width()/2, image.get_height()/2)
 
 class Asteroid(GameObject):
 	
-	ASTEROID_L1 = Rect(0, 0, 200, 200)
-	ASTEROID_L2 = Rect(200, 0, 185, 200)
-	ASTEROID_L3 = Rect(400, 0, 200, 200)
-	ASTEROID_M1 = Rect(0, 200, 200, 200)
-	ASTEROID_M2 = Rect(0, 200, 200, 200)
-	ASTEROID_M3 = Rect(0, 200, 200, 200)
-	ASTEROID_S1 = Rect(0, 200, 200, 200)
-	ASTEROID_S2 = Rect(0, 200, 200, 200)
-	ASTEROID_S3 = Rect(0, 200, 200, 200)
+	ASTEROID_L1 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_L1.png"))#.convert()
+	ASTEROID_L2 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_L2.png"))#.convert()
+	ASTEROID_L3 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_L3.png"))#.convert()
+	ASTEROID_M1 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_M1.png"))#.convert()
+	ASTEROID_M2 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_M2.png"))#.convert()
+	ASTEROID_M3 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_M3.png"))#.convert()
+	ASTEROID_S1 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_S1.png"))#.convert()
+	ASTEROID_S2 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_S2.png"))#.convert()
+	ASTEROID_S3 = Image.load(os.path.join(os.path.dirname(__file__), r"sprites", r"asteroid_S3.png"))#.convert()
 
 	ASTEROIDS_LARGE = (ASTEROID_L1, ASTEROID_L2, ASTEROID_L3)
 	ASTEROIDS_MEDIUM = (ASTEROID_M1, ASTEROID_M2, ASTEROID_M3)
 	ASTEROIDS_SMALL = (ASTEROID_S1, ASTEROID_S2, ASTEROID_S3)
 
-	def __init__(self, pos: tuple, size: int=2) -> None:
-		super().__init__(pos)
+	def __init__(self, pos: tuple, velocity: tuple, rotation: float, rot_velocity: float, size: int=2) -> None:
+		super().__init__(pos, rotation, velocity)
 		self.size = size
-		self.IMAGE_LOCATION = self._assign_image_location(size)
+		self._IMAGE = self._assign_image(size)
+		self._IMAGE.set_colorkey(self._IMAGE.get_at((0, 0)))
 
-	def _assign_image_location(self, size: int) -> Rect:
+	def _assign_image(self, size: int) -> Surface:
 		'''Assigns a sprite location based on the size of the asteroid'''
 		match size:
 			case 2:
-				return Asteroid.ASTEROID_L1
+				return random.choice(Asteroid.ASTEROIDS_LARGE)
 			case 1:
 				return random.choice(Asteroid.ASTEROIDS_MEDIUM)
 			case _:
 				return random.choice(Asteroid.ASTEROIDS_SMALL)
 
+	def get_image(self):
+
+		return rotate(self._IMAGE, self.rotation)
+
 class Saucer(GameObject):
 	
-	def __init__(self, pos: tuple) -> None:
-		super().__init__(pos)
+	SAUCER_2 = Rect(34, 535, 71, 134)
+	SAUCER_3 = Rect(175, 514, 147, 147)
+
+	def __init__(self, pos: tuple, velocity: tuple, rotation: float, rot_velocity: float) -> None:
+		super().__init__(pos, rotation, velocity)
+		self.IMAGE_LOCATION = Saucer.SAUCER_3#random.choice((Saucer.SAUCER_2, Saucer.SAUCER_3))
 
 def main():
-	p = Player((0,0))
-	print(p.IMAGE, p.IMAGE_LOCATION, p.pos)
-	a = Asteroid((1,1), 2)
-	print(a.IMAGE, a.IMAGE_LOCATION, a.pos)
+	pass
 
 if __name__ == '__main__':
 	main()
--- games/asteroids/asteroids.py
+++ games/asteroids/asteroids.py
@@ -4,32 +4,35 @@ import sys
 from asteroid_classes import Player
 from asteroid_classes import Asteroid
 from asteroid_classes import Saucer
+from pygame.math import Vector2
 
 pygame.init()
 
 #screen format
 ratio = 4, 3
-factor = 1080/ratio[0]
+factor = 1440/ratio[0]
 
 width, height = ratio[0]*factor, ratio[1]*factor
 screen = pygame.display.set_mode((width, height))
-p = Player((100, 100))
-a = Asteroid((200,200))
+p = Player((0, 0), (50, 50), 0, 50)
 
 #backround format
-background_color = pygame.Color('grey')
+background_color = pygame.Color('darkolivegreen')
 
 #clock format
 clock = pygame.time.Clock()
 
 #display loop
 while True:
+
 	if pygame.event.get(pygame.QUIT):
 		sys.exit()
 
 	#handles displaying and refreshing the screen after each evauluation
-	clock.tick()
+
+	p.update(clock.get_time())
+
+	clock.tick(144)
 	screen.fill(background_color)
-	screen.blit(p.IMAGE, p.pos, p.IMAGE_LOCATION)
-	screen.blit(a.IMAGE, a.pos, a.IMAGE_LOCATION)
+	screen.blit(p.get_image(), p.get_pos())
 	pygame.display.update()
