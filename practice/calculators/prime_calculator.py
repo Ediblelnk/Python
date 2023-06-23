c018c4afaa86f1087e403c675bac67152f5b01c8
Ediblelnk <peters1731@gmail.com>
2023-06-22 22:49:42 -0400

    cleaned up prime calc

--- practice/calculators/prime_calculator.py
+++ practice/calculators/prime_calculator.py
@@ -6,20 +6,8 @@ def functionTimer(function):
 	def timeIt(*args, **kwarg):
 		start = time.time()
 		function(*args, **kwarg)
-		print(time.time()-start)
+		print((time.time()-start)*1000, "ms")
 	return timeIt
-
-#prints primes in input range
-@functionTimer
-def brutePrimeIn(bound):
-	total = 0
-	for num in range(2, bound):
-		for i in range(2, num):
-			if num % i == 0:
-				break
-		else:
-			total += 1
-	print('brutePrimeIn', total, sep=', ')
 
 @functionTimer
 def betterPrimeIn(bound):
@@ -32,17 +20,6 @@ def betterPrimeIn(bound):
 			total += 1
 	print('betterPrimeIn', total, sep=', ', end='; ')
 
-@functionTimer
-def listPrimeIn(bound):
-	primelist = [2]
-	for num in range(3, bound, 2):
-		for prime in primelist:
-			if num%prime == 0:
-				break
-		else:
-			primelist.append(num)
-	print('listPrimeIn', len(primelist), sep=', ', end='; ')
-
 
 #returns if a num is prime or not
 def isPrime(num):
@@ -52,5 +29,4 @@ def isPrime(num):
 	else:
 		return False
 
-betterPrimeIn(100000)
-listPrimeIn(100000)
+betterPrimeIn(1_000_000)
